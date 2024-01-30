54641b5f0e762f2c1c71e61da406369db1b0a1a2
erastoff <yuriy.erastov@yandex.ru>
2024-01-30 17:38:42 +0300

    Master - settings.py fixed

--- hasker/settings.py
+++ hasker/settings.py
@@ -75,12 +75,6 @@ WSGI_APPLICATION = "hasker.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
@@ -129,7 +123,6 @@ AUTH_USER_MODEL = "users.User"
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
