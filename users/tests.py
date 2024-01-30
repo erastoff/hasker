a050ace011366fb51123a85ae038385a5f4dbcee
erastoff <yuriy.erastov@yandex.ru>
2024-01-30 18:20:46 +0300

    Master - simple users test implemented

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,15 @@
-# from django.test import TestCase
+from django.test import TestCase
 
-# Create your tests here.
+from users.models import User
+
+
+class UserModelTest(TestCase):
+    def test_create_user(self):
+        user = User.objects.create(
+            email="test@example.com",
+            username="testuser",
+            password="testpassword",
+        )
+
+        self.assertEqual(user.email, "test@example.com")
+        self.assertEqual(user.username, "testuser")
