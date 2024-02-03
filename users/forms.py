5af16b9a55efc0d415d226edcf72cb8fa5019f85
erastoff <yuriy.erastov@yandex.ru>
2024-02-03 20:00:09 +0300

    Master - some users views added

---
+++ users/forms.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django import forms
+from django.forms import Widget
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class RegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    username = forms.CharField(help_text="Enter your desired username")
+    password1 = forms.CharField(
+        help_text="Enter your passsword", widget=forms.PasswordInput(), label="Password"
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.items():
+            widget: Widget = field[1].widget
+            widget.attrs["class"] = "form-control"
+
+
+class UserAuthenticationForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ("username", "password")
+
+    username = forms.CharField(help_text="")
+    # password1 = forms.CharField(help_text='', widget=forms.PasswordInput(), label='Password')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.items():
+            widget: Widget = field[1].widget
+            widget.attrs["class"] = "form-control"
--- users/views.py
+++ users/views.py
@@ -1,3 +1,38 @@
 # from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView
 
-# Create your views here.
+from users.forms import RegistrationForm, UserAuthenticationForm
+
+
+class RegistrationView(CreateView):
+    model = User
+    # fields = ('username', 'email', 'password')
+    form_class = RegistrationForm
+    success_url = reverse_lazy("users:login")
+    success_message = "Поздравляем! Вы успешно зарегистрированы!"
+    template_name = "users/user_form.html"
+
+
+class UserLoginView(LoginView):
+    template_name = "users/login_form.html"
+    form_class = UserAuthenticationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy("index")
+
+
+class UserLogoutView(LogoutView):
+    pass
+    # template_name = "users/login_form.html"
+    # success_url = reverse_lazy("index")
+
+
+class UserTemplateView(TemplateView):
+    template_name = "users/user_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["object"] = self.request.user
+        return context
