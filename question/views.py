93fc51b309c2413902c66380f4ac8b2622f6ae10
erastoff <yuriy.erastov@yandex.ru>
2024-02-04 18:15:05 +0300

    Master - buttons added to the question list page

--- question/views.py
+++ question/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.views.generic import ListView
 
 from question.models import Question
@@ -6,3 +7,16 @@ from question.models import Question
 class QuestionListView(ListView):
     model = Question
     template_name = "question/index.html"
+    paginate_by = 20
+
+    def post(self, request):
+        filter_option = request.POST.get("filter_option")
+        active_button = request.POST.get("filter_option", "option1")
+        if filter_option == "option1":
+            questions = Question.objects.order_by("-created_at")
+        elif filter_option == "option2":
+            questions = Question.objects.order_by("created_at")
+        else:
+            questions = Question.objects.all()
+        context = {"question_list": questions, "active_button": active_button}
+        return render(request, self.template_name, context)
