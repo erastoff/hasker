40ee513b787c2bd406d5336436e099ab3d95745b
erastoff <yuriy.erastov@yandex.ru>
2024-02-13 15:01:10 +0300

    Master - "Hot question" sorting added

--- question/views.py
+++ question/views.py
@@ -29,7 +29,7 @@ class QuestionListView(ListView):
                 num_answers=Count("answers")
             )
         elif filter_option == "option2":
-            questions = Question.objects.order_by("created_at").annotate(
+            questions = Question.objects.order_by("-votes").annotate(
                 num_answers=Count("answers")
             )
         else:
