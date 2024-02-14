316e3e59333fa5c9e9a92eefc5c9cb77c9d86037
erastoff <yuriy.erastov@yandex.ru>
2024-02-14 21:46:52 +0300

    Master - static serving and image field in navbar added

--- hasker/urls.py
+++ hasker/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -24,3 +26,7 @@ urlpatterns = [
     path("", include("question.urls", namespace="question")),
     path("users/", include("users.urls", namespace="users")),
 ]
+
+if settings.DEBUG:
+    # urlpatterns.append(path("__debug__/", include("debug_toolbar.urls")))
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
