f5e8e309a17dd21adb1ef35fbc84ee94ff209658
erastoff <yuriy.erastov@yandex.ru>
2024-02-22 09:25:49 +0300

    Master - question API search added

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
 
-from api.views import QuestionListAPIView
-from api.views import QuestionDetailAPIView
+from api.views import (
+    QuestionListAPIView,
+    QuestionSearchAPIView,
+    QuestionDetailAPIView,
+    QuestionTagSearchAPIView,
+)
 
 app_name = "api"
 
 urlpatterns = [
     path("question_list/", QuestionListAPIView.as_view(), name="question-list"),
     path("question/<int:pk>/", QuestionDetailAPIView.as_view(), name="question-api"),
+    path("search/", QuestionSearchAPIView.as_view(), name="question_search-api"),
+    path(
+        "tag/<str:tag_word>/",
+        QuestionTagSearchAPIView.as_view(),
+        name="question_tag_search_api",
+    ),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,5 @@
+from django.db.models import Q, Count
 from rest_framework.generics import ListAPIView, RetrieveAPIView
-
-# from rest_framework.viewsets import ModelViewSet
 
 from question.models import Question
 from question.serializers import QuestionListSerializer, QuestionDetailSerializer
@@ -15,3 +14,32 @@ class QuestionDetailAPIView(RetrieveAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionDetailSerializer
     lookup_field = "pk"
+
+
+class QuestionSearchAPIView(ListAPIView):
+    serializer_class = QuestionListSerializer
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        if query:
+            return (
+                Question.objects.filter(
+                    Q(title__icontains=query) | Q(content__icontains=query)
+                )
+                .annotate(num_answers=Count("answers"))
+                .order_by("-votes", "-created_at")
+            )
+        return Question.objects.none()
+
+
+class QuestionTagSearchAPIView(ListAPIView):
+    serializer_class = QuestionListSerializer
+
+    def get_queryset(self):
+        tag_word = self.kwargs.get("tag_word")
+        queryset = (
+            Question.objects.filter(tags__tag_word=tag_word)
+            .annotate(num_answers=Count("answers"))
+            .order_by("-votes", "-created_at")
+        )
+        return queryset
